af21a827a28a4c78a62f8f1bc6c6e81b92fbb508
Nymito <gianni.jouve@gmail.com>
2020-05-23 19:34:13 +0100

    AssetsSpace + BG

---
+++ Assets/Scripts/LoopOffsetBg.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopOffsetBg : MonoBehaviour
+{
+  
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        
+
+        
+
+        
+        offset.y = GameObject.FindGameObjectWithTag("Player").transform.position.y / 100;
+
+        offset.x += Time.deltaTime / 10f;
+
+        mat.mainTextureOffset = offset;
+
+
+    }
+}
